c740e59717a2dcd6134c7ec9d90b53c361cf16b5
Morgan Haskel <morgan@puppetlabs.com>
2015-09-14 11:36:53 -0700

    Add http_authorization provider for hocon_setting
    This will allow us to sort the rules by sort_order with a secondary sort
    on name.

---
+++ lib/puppet/provider/hocon_setting/http_authorization.rb
@@ -0,0 +1,26 @@
+Puppet::Type.type(:hocon_setting).provide(:http_authorization, :parent => Puppet::Type.type(:hocon_setting).provider(:ruby)) do
+  def set_value(value_to_set)
+    if resource[:type] == 'array_element'
+      tmp_val = []
+      val = value
+      Array(val).each do |v|
+        tmp_val << v
+      end
+      Array(value_to_set).each do |v|
+        unless tmp_val.include?(v)
+          tmp_val << v
+        end
+      end
+
+      tmp_val.sort_by! { |rule| [rule['sort_order'], rule['name']] }
+
+      new_value = Hocon::ConfigValueFactory.from_any_ref(tmp_val, nil)
+
+      conf_file_modified = conf_file.set_config_value(setting, new_value)
+
+      return conf_file_modified
+    else
+      super
+    end
+  end
+end
