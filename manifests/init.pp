e91621712fd68e5b4c66ded89c5a3a148ef47360
Ramesh Sencha <ramesh.sencha@perforce.com>
2023-10-20 15:58:56 +0530

    CAT-1539 - Running PDK update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,24 @@
+#
+# @summary Define type to manage the puppetserver authorization
+#
+# @param version [Integer]
+#   The version of the authorization
+#
+# @param allow_header_cert_info [Boolean]
+#   Whether to allow header cert info
+#
+# @param replace [Boolean]
+#   Whether to replace the file
+#
+# @param path [Stdlib::Absolutepath]
+#   The path to the file
+#
 define puppet_authorization (
   Integer $version = 1,
   Boolean $allow_header_cert_info = false,
   Boolean $replace = false,
   Stdlib::Absolutepath $path = $name,
-){
+) {
   concat { $name:
     path    => $path,
     replace => $replace,
